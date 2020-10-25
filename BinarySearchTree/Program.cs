bcff2ad8567d408b41ffc9c692166de78fd7ae80
Kumar Kartikeya <kartikeya.dhoni@gmail.com>
2020-10-25 23:24:22 +0530

    [Kartikeya] UC added to search for a particular item

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -21,6 +21,7 @@ namespace BinarySearchTree
             myBinaryTree.Insert(63);
             myBinaryTree.Insert(67);
             myBinaryTree.Display();
+            myBinaryTree.Search(40);
         }
     }
 }
